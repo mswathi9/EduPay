404ac663e830616bae6cc2d691b514a405be9327
K.S.S.V.K.SATISH RAJU <satishraju3548@gmail.com>
2025-12-24 10:47:32 +0530

    added backend uri

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Force local development URL for debugging
-const baseURL = 'http://localhost:5001/api';
+const baseURL = 'https://bvce-edupay-backend.onrender.com';
 
 console.log('API Base URL (Forced):', baseURL);
 
